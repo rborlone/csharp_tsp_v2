fb95fd9899bae4a5f0d2d98887b8637d9b0f8511
Rodrigo Borlone Espoz <rodrigo.borlone@gmail.com>
2024-05-13 16:43:02 -0400

    Se agrega funcion 3opt

--- tspConsoleApp/Tour.cs
+++ tspConsoleApp/Tour.cs
@@ -201,6 +201,78 @@ class Tour
         return 0;
     }
 
+        public int ThreeOpt(Random engine, Mapa mapa, Tour mejor = null)
+    {
+        //int aleatorio = engine.Next(0, largo - 1); // RandomNumberGenerator.Instance.Generate(0, largo -1);
+
+        int aleatorio = (int)RandomNumberGenerator.Instance.Generate(0, largo -1); 
+
+        Ciudad inicio = ady[0];
+        var t0 = ady[aleatorio];
+        Ciudad t1 = null;
+
+        while(true) //optimización aplicada a un mejor tour
+        {
+            t1 = t0.siguiente;
+            if (mejor == null) 
+                break;
+            if (mejor.ady[t0.idCiudad].siguiente.idCiudad != t1.idCiudad && mejor.ady[t0.idCiudad].anterior.idCiudad != t1.idCiudad) 
+                break;
+            t0 = t0.siguiente;
+
+            if (t0 == inicio)
+                return 0;
+        }
+
+         int g0=mapa.data[t0.idCiudad][t1.idCiudad];
+
+        foreach (var nodo in mapa.candidatosDeNodo[t1.idCiudad])
+        {
+            var t2 = ady[nodo];
+            int G0=g0-mapa.data[t1.idCiudad][t2.idCiudad];
+            if (G0 <= 0 || t1.siguiente == t2 || t1.anterior == t2) 
+                continue; 
+            var t3 = t2.anterior;
+            int g1 = G0+mapa.data[t2.idCiudad][t3.idCiudad]-mapa.data[t3.idCiudad][t0.idCiudad];
+            if (g1 > 0){
+                int t3t1 = t3.posicion - t1.posicion;
+                int t0t2 = t0.posicion - t2.posicion;
+                if (t3t1 < 0) 
+                    t3t1 += ady.Length;
+
+                if (t0t2 < 0)
+                    t0t2 += ady.Length;
+
+                if (t3t1<t0t2)
+                    Mover(t0, t1, t2, t3); //Movemos en el sentido horario.
+                else
+                    Mover(t3, t2, t1, t0); //Movemos en el sentido contra sentido horario.
+                return g1;    
+            } else {
+                //3opt
+                foreach (var opt3 in mapa.candidatosDeNodo[t3.idCiudad]){
+                    var t4 = ady[opt3];
+
+                    int G1=G0+mapa.data[t2.idCiudad][t3.idCiudad]-mapa.data[t3.idCiudad][t4.idCiudad];
+                    
+                    if (G1<=0 || t4==t3.siguiente || t4==t3.siguiente)
+                        continue;
+
+                    bool post4=Entre(t1, t3, t4);
+                        Ciudad t5=post4?t4.siguiente:t4.anterior;
+
+                        int g2=G1+mapa.data[t4.idCiudad][t5.idCiudad]-mapa.data[t5.idCiudad][t0.idCiudad];
+                        if (g2>0){
+                            Mover(t0,t1, t2, t3);
+                            Mover(t0,t3, t4, t5);
+                            return g2;
+                        }
+                }
+            }   
+        }
+        return 0;
+    }
+
     /***
         Metodo que realizar un swap de ciudades por referencias.
     */
